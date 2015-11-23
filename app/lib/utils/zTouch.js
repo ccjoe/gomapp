734b3bb0df2ee8caa83205b6b8dfae0e1e1713d8
ccjoe <icareu.joe@gmail.com>
2015-11-23 23:48:51 +0800

    nothin

--- app/lib/utils/zTouch.js
+++ app/lib/utils/zTouch.js
@@ -1,48 +1,86 @@
 /*
  * swipe
  */
-function Swipe(tBox, args) {
-    var tPoint = {
-        self: tBox,
+/*
+* * 主要提供以下回调函数：
+ beforeCallback (实例化之前触发的回调函数),
+ sCallback (start callback,touchstart时触发的回调函数),
+ mCallback (move callback,touchmove时触发的回调函数),
+ eCallback (end callback,touchend时触发的回调函数)
+ afterCallback (实例化之后触发的回调函数),
+
+ * 回调函数中会传入touch事件相关回调参数：
+ self:实例化的BOX,
+ startX:触点起始X,
+ startY:触点起始Y,
+ boxLeft:(Box Left) 相对于文档的left偏移,
+ boxTop:(Box Top)相对于文档的top偏移,
+ boxWidth:(Box Width)BOX的宽度,
+ boxHeight:(Box Height)BOX的高度,
+ boxRightRange:(Box Right Border)BOX的右边界,
+ boxBottomRange:(Box Bottom Border)BOX的下边界,
+ endX:触点结束X;
+ endY:触点结束Y;
+ moveX:(move x distance)X方向滑动距离;
+ moveY:(move y distance)Y方向滑动距离;
+ direction:手势滑动方向(left/right/false);
+ angle:单点手势滑动角度;
+ duration:手势持续时间;
+ vendor:浏览器前缀(-moz/-webkit/-o/-ms);
+
+
+ touchPoint.multiTouch:是否多点触摸(touchmove时可监控);
+ touchPoint.gStartDis:(gisture start distance)手势起始距离;
+ touchPoint.gEndDis:(gisture end distance)手势结束距离;
+ touchPoint.scale:手势缩放比例;
+ touchPoint.rotation:手势旋转角度;
+
+ * 同时在外部回调函数中提供修改内部touchPoint数据的接口:
+ touchPoint.setAttr(name,value);
+ * 调用方法:
+ */
+function Swipe(touchBox, opts) {
+    var touchPoint = {
+        self: touchBox,
         count: 0,
         speed: 300,
-        iniL: 30,
-        iniT: 30,
-        iniAngle: 30,
+        minX: 30,       //X方向滑动的最小距离
+        maxY: 30,       //Y方向滑动的最大距离
+        initAngle: 30,  //最小满足角度
         touch: false,
-        mutiTouch: false,
+        multiTouch: false,  //多点触控检测
         setAttr: function (name, value) {
-            tPoint[name] = value;
+            touchPoint[name] = value;
         }
     };
 
     // init初始化
     (function () {
-        _this = tPoint.self;
-        for (var o in args) {
-            tPoint[o] = args[o];
+        that = touchPoint.self;
+        for (var o in opts) {
+            touchPoint[o] = opts[o];
         }
-        var _offset = _this.offset();
-        tPoint.bL = _offset.left;
-        tPoint.bT = _offset.top;
-        tPoint.bW = _this.width();
-        tPoint.bH = _this.height();
-        tPoint.bRb = tPoint.bL + tPoint.bW; //右边界
-        tPoint.bBb = tPoint.bT + tPoint.bH; //下边界
-        tPoint.total = _this.children().children().length;
+        var offset = that.offset();
+        touchPoint.boxLeft = offset.left;
+        touchPoint.boxTop = offset.top;
+        touchPoint.boxWidth = that.width();
+        touchPoint.boxHeight = that.height();
+        touchPoint.boxRightRange = touchPoint.boxLeft + touchPoint.boxWidth; //右边界
+        touchPoint.boxBottomRange = touchPoint.boxTop + touchPoint.boxHeight; //下边界
+        touchPoint.total = that.children().children().length;
     })();
 
     // 浏览器特性检测
-    tPoint.vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' :
+    touchPoint.vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' :
         (/firefox/i).test(navigator.userAgent) ? 'Moz' :
             'opera' in window ? 'O' : '';
-    tPoint.has3d = 'WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix();
-    tPoint.hasTouch = 'ontouchstart' in window;
-    tPoint.hasTransform = tPoint.vendor + 'Transform' in document.documentElement.style;
+    touchPoint.has3d = 'WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix();
+    touchPoint.hasTouch = 'ontouchstart' in window;
+    touchPoint.hasTransform = touchPoint.vendor + 'Transform' in document.documentElement.style;
 
     // 方向检测(左移距离，上移距离)
     function directionDetect(l, t) {
-        if (Math.abs(t) < tPoint.iniT && Math.abs(l) > tPoint.iniL) {
+        if (Math.abs(t) < touchPoint.maxY && Math.abs(l) > touchPoint.minX) {
             var rStr = l < 0 ? 'left' : 'right';
             return rStr;
         }
@@ -51,7 +89,7 @@ function Swipe(tBox, args) {
 
     // 边界检测
     function borderDetect(x, y) {
-        return (x < tPoint.bL || x > tPoint.bRb || y < tPoint.bT || y > tPoint.bBb);
+        return (x < touchPoint.boxLeft || x > touchPoint.boxRightRange || y < touchPoint.boxTop || y > touchPoint.boxBottomRange);
     }
 
     // 获取夹角(通过arctant反三角函数)
@@ -64,52 +102,52 @@ function Swipe(tBox, args) {
         return Math.sqrt(Math.pow(xLen, 2) + Math.pow(yLen, 2));
     }
 
-    // 设置tPoint的改变数据（默认设置改变数据，如果setList存在，则遍历setList设置属性）
-    function setPointData(point, setList) {
+    // 设置touchPoint的改变数据（默认设置改变数据，如果setList存在，则遍历setList设置属性）
+    function setouchPointData(point, setList) {
         if (setList) {
             for (var o in setList) {
-                tPoint.setAttr(o, setList[o]);
+                touchPoint.setAttr(o, setList[o]);
             }
         } else {
             var t = new Date();
-            tPoint.endX = point.pageX;
-            tPoint.endY = point.pageY;
-            tPoint.endTime = t.getTime();
-            tPoint.duration = tPoint.endTime - tPoint.startTime;
-            tPoint.mX = point.pageX - tPoint.startX;//滑动距离
-            tPoint.mY = point.pageY - tPoint.startY;
-            tPoint.direction = directionDetect(tPoint.mX, tPoint.mY);
-            tPoint.angle = getAngle(tPoint.mY / tPoint.mX);
+            touchPoint.endX = point.pageX;
+            touchPoint.endY = point.pageY;
+            touchPoint.endTime = t.getTime();
+            touchPoint.duration = touchPoint.endTime - touchPoint.startTime;
+            touchPoint.moveX = point.pageX - touchPoint.startX;//滑动距离
+            touchPoint.moveY = point.pageY - touchPoint.startY;
+            touchPoint.direction = directionDetect(touchPoint.moveX, touchPoint.moveY);
+            touchPoint.angle = getAngle(touchPoint.moveY / touchPoint.moveX);
         }
     }
 
     // 多点触摸检测(只检测两点触摸)
     function multiTouchDetect(e) {
-        tPoint.tLen = e.touches.length;
-        if (tPoint.tLen > 1) {
+        touchPoint.tLen = e.touches.length;
+        if (touchPoint.tLen > 1) {
             var point0 = e.touches[0],
                 point1 = e.touches[1],
                 xLen = point1.pageX - point0.pageX,
                 yLen = point1.pageY - point0.pageY,
                 angle = getAngle(yLen / xLen),
                 gDis = getDis(xLen, yLen);
-            if (!tPoint.mutiTouch) {
-                tPoint.gStartDis = gDis;
-                tPoint.gStartAngle = angle;
+            if (!touchPoint.multiTouch) {
+                touchPoint.gStartDis = gDis;
+                touchPoint.gStartAngle = angle;
             } else {
-                tPoint.gEndDis = gDis;
-                tPoint.gEndAngle = angle;
-                tPoint.scale = gDis / tPoint.gStartDis;
-                tPoint.rotation = angle - tPoint.gStartAngle;
+                touchPoint.gEndDis = gDis;
+                touchPoint.gEndAngle = angle;
+                touchPoint.scale = gDis / touchPoint.gStartDis;
+                touchPoint.rotation = angle - touchPoint.gStartAngle;
             }
-            tPoint.mutiTouch = true;
+            touchPoint.multiTouch = true;
         } else {
-            tPoint.mutiTouch = false;
+            touchPoint.multiTouch = false;
         }
         ;
     }
 
-    function startFun(e) {
+    function startCallback(e) {
         var point = e.touches[0],
             t = new Date();
         var setList = {
@@ -118,53 +156,53 @@ function Swipe(tBox, args) {
             startTime: t.getTime(),
             identifier: point.identifier
         }
-        setPointData(point, setList);
-        tPoint.touch = true;
-        if (typeof tPoint.sCallback == "function") {
-            tPoint.sCallback(tPoint);
+        setouchPointData(point, setList);
+        touchPoint.touch = true;
+        if (typeof touchPoint.sCallback == "function") {
+            touchPoint.sCallback(touchPoint);
         }
     }
 
-    function moveFun(e) {
+    function moveCallback(e) {
         var point = e.touches[0];
         if (borderDetect(point.pageX, point.pageY)) {
-            tPoint.touch = false;
+            touchPoint.touch = false;
             return false;
         }
-        if (tPoint.touch) {
-            setPointData(point);
+        if (touchPoint.touch) {
+            setouchPointData(point);
             multiTouchDetect(e);// Muti touch detect
-            if (typeof tPoint.mCallback == "function") {
-                tPoint.mCallback(tPoint);
+            if (typeof touchPoint.mCallback == "function") {
+                touchPoint.mCallback(touchPoint);
             }
         }
-        if (Math.abs(tPoint.angle) < tPoint.iniAngle) {
+        if (Math.abs(touchPoint.angle) < touchPoint.initAngle) {
             e.preventDefault();
         }
     }
 
-    function endFun(e) {
-        tPoint.touch = false;
-        tPoint.mutiTouch = false;
-        if (typeof tPoint.eCallback == "function") {
-            tPoint.eCallback(tPoint);
+    function endCallback(e) {
+        touchPoint.touch = false;
+        touchPoint.multiTouch = false;
+        if (typeof touchPoint.eCallback == "function") {
+            touchPoint.eCallback(touchPoint);
         }
     }
 
     // 实例化之前的回调函数
-    if (typeof tPoint.beforeCallback == "function") {
-        tPoint.beforeCallback(tPoint);
+    if (typeof touchPoint.beforeCallback == "function") {
+        touchPoint.beforeCallback(touchPoint);
     }
 
     // Touch事件监听
-    _this.die("touchstart,touchmove,touchend");
-    _this.get(0).addEventListener('touchstart', startFun);
-    _this.get(0).addEventListener('touchmove', moveFun);
-    _this.get(0).addEventListener('touchend', endFun);
+    that.die("touchstart,touchmove,touchend");
+    that.get(0).addEventListener('touchstart', startCallback);
+    that.get(0).addEventListener('touchmove', moveCallback);
+    that.get(0).addEventListener('touchend', endCallback);
 
     // 实例化之后的回调函数
-    if (typeof tPoint.afterCallback == "function") {
-        tPoint.afterCallback(tPoint);
+    if (typeof touchPoint.afterCallback == "function") {
+        touchPoint.afterCallback(touchPoint);
     }
 }
 
