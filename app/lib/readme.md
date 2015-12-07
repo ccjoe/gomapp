9d558dfb941d0fac65b2255d9448e7a273404091
unknown <sfliu@DST89414.cn3.global.ctrip.com>
2015-12-07 18:23:07 +0800

    improve whole ref

---
+++ app/lib/readme.md
@@ -0,0 +1,7 @@
+gom is Go Mobile!
+TODO LIST
+
+可视化绘制出依赖关系图
+可定制ui组件，build时可获取最优化版本
+Build Dist版本
+完善文档及发布
--- app/lib/ui/ui.js
+++ app/lib/ui/ui.js
@@ -4,6 +4,9 @@ define(function(require){
         Header: require('base/ui/ui.header'),
         List:   require('base/ui/ui.list'),
         Modal:  require('base/ui/ui.modal'),
-        Sides:  require('base/ui/ui.sides')
+        Sides:  require('base/ui/ui.sides'),
+        Scroll:  require('base/ui/ui.scroll'),
+        Slide:  require('base/ui/ui.slide'),
+        Select: require('base/ui/ui.select')
     }
 });
