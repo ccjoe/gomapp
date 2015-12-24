796e897aabe0f18d85bd045fde266ca94937f5a4
unknown <icareu.joe@gmail.com>
2015-12-24 16:27:01 +0800

    build app

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
-var GOM_PATH = __dirname + '/gom/';
+var GOM_PATH = __dirname + '/app/gom/';
 var TEMPLATE_EXT_REGEX = /\.(html|tmpl)$/;
 var TEMPLATE_BASE_PATH = GOM_PATH + 'src/ui/ui.tmpl/';
 var TEMPLATE_OUT_PATH = GOM_PATH + 'src/ui/';
@@ -59,45 +59,37 @@ gulp.task('gom-lib', function () {
 /*------------- RequireJs  ------------*/
 var exec = require('child_process').exec;
 gulp.task('gom-scripts', function () {
-    exec('node ./gom/r.js -o ./gom/r-config.js', function (err, stdout, stderr) {
+    exec('node ./app/gom/r.js -o ./app/gom/r-config.js', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
+        gulp.start('gom-lib');
         //以下内容执行的话将合并gom.js与base.js
-       /* gulp.src([GOM_PATH + 'src/3rd/zepto.js', GOM_PATH + 'src/3rd/!(zepto.js)*.js', GOM_PATH+'build/gom.js'])
-            .pipe(uglify())
-            .pipe(concat('gom.js'))
-            .pipe(gulp.dest(GOM_PATH + 'build/'));*/
+        /* gulp.src([GOM_PATH + 'src/3rd/zepto.js', GOM_PATH + 'src/3rd/!(zepto.js)*.js', GOM_PATH+'build/gom.js'])
+         .pipe(uglify())
+         .pipe(concat('gom.js'))
+         .pipe(gulp.dest(GOM_PATH + 'build/'));*/
     });
 });
-
-/*-------------- FONTS -----------------*/
-//, 'gom-fonts'
-/*gulp.task('gom-fonts', function () {
- return gulp.src(GOM_PATH+'fonts/').concat(GOM_PATH+'app/fonts/!**!/!*')
- .pipe(gulp.dest('.tmp/fonts'))
- .pipe(gulp.dest(GOM_PATH+'build/fonts'));
- });*/
 
 /*------------- Documents  ------------*/
 var docs_exec = require('child_process').exec;
 gulp.task('gom-docs', function(){
-   docs_exec('jsdoc -t ../minami -c "./docs-conf.json" -r ./gom/src/ --readme ./gom/readme.md -d ./gom/docs')
+    docs_exec('jsdoc -t ../minami -c "./docs-conf.json" -r ./app/gom/src/ --readme ./app/gom/readme.md -d ./app/gom/docs')
 });
 
 gulp.task('gom', ['gom-preCompiler', 'gom-scripts'], function () {
     gulp.start('gom-scss');
-    gulp.start('gom-lib');
     gulp.start('gom-docs');
 });
 
 /*----------------------GOM Frame Build END-------------------------*/
 
-/*--------------------APP EXAMPLE BUILD BEGIN-----------------------*/
+/*--------------------APP EXAMPLE DEV AND BUILD BEGIN-----------------------*/
 /*--------------------- SASS ----------------*/
-var sass = require('gulp-ruby-sass');
 gulp.task('styles', function () {
-    return sass('./app/styles/main.scss')
-        .pipe(gulp.dest('./app/css/'));
+    return sass('app/styles/main.scss')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('app/css/'));
 });
 
 /*--------------------- JS ----------------*/
@@ -109,19 +101,31 @@ gulp.task('jshint', function () {
     // .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
+//将gom build后的版本移至dist目录 ['gom'],
+gulp.task('move-gom',  function () {
+    return gulp.src('app/gom/build/**/*.js').pipe(gulp.dest('dist/gom/build/'));
+});
+
+gulp.task('scripts', ['move-gom'],  function () {
+    exec('node  r.js -o  r-config.js', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
+});
+
 /*--------------------- HTML ----------------*/
 //remove useref;
 gulp.task('html', ['styles'], function () {
-    //var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+    var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
 
-    return gulp.src('app/*.html')
-        //.pipe(assets)
+    return gulp.src(['app/**/*.html', '!app/gom/**/*.html'])
+        .pipe(assets)
         //.pipe($.if('*.js', $.uglify()))
-        //.pipe($.if('*.css', $.csso()))
-        //.pipe(assets.restore())
-        //.pipe($.useref())
+        .pipe($.if('*.css', $.csso()))
+        .pipe(assets.restore())
+        .pipe($.useref())
         .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/'));
 });
 
 /*-------------------- IMAGES ----------------*/
@@ -139,13 +143,15 @@ gulp.task('images', function () {
 
 /*-------------------- FONTS -----------------*/
 gulp.task('fonts', function () {
-    return gulp.src(require('main-bower-files')({
-        filter: '**/*.{eot,svg,ttf,woff,woff2}'
-    }).concat('app/fonts/**/*'))
+    return gulp.src('app/gom/src/fonts/**/*.{eot,svg,ttf,woff,woff2}')//.concat('app/fonts/**/*'))
         .pipe(gulp.dest('.tmp/fonts'))
         .pipe(gulp.dest('dist/fonts'));
 });
-
+/*gulp.task('gom-fonts', function () {
+ return gulp.src(GOM_PATH+'fonts/').concat(GOM_PATH+'app/fonts/!**!/!*')
+ .pipe(gulp.dest('.tmp/fonts'))
+ .pipe(gulp.dest(GOM_PATH+'build/fonts'));
+ });*/
 /*-------------------- EXTRAS -----------------*/
 gulp.task('extras', function () {
     return gulp.src([
@@ -157,9 +163,25 @@ gulp.task('extras', function () {
 });
 /*--------------------APP EXAMPLE BUILD END-----------------------*/
 
+// inject bower components
+/*--------------------- DEV ----------------*/
+/*gulp.task('wiredep', function () {
+ var wiredep = require('wiredep').stream;
+
+ gulp.src('app/styles/!*.scss')
+ .pipe(wiredep({
+ ignorePath: /^(\.\.\/)+/
+ })).pipe(gulp.dest('app/styles'));
+
+ gulp.src('app/!*.html')
+ .pipe(wiredep({
+ ignorePath: /^(\.\.\/)*\.\./
+ })).pipe(gulp.dest('app'));
+ });*/
+
 /*--------------------- SERVER ----------------*/
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
-gulp.task('serve', ['styles', 'fonts'], function () {
+gulp.task('serve', ['styles'], function () {
     browserSync({
         //notify: false,
         port: 9000,
@@ -167,7 +189,7 @@ gulp.task('serve', ['styles', 'fonts'], function () {
             baseDir: ['.tmp', 'app'],
             routes: {
                 '/bower_components': 'bower_components',
-                '/gom': 'gom'
+                //'/gom': 'gom' 不再提供此二个目录的路由
             }
         },
         browser: ['chrome']
@@ -175,45 +197,31 @@ gulp.task('serve', ['styles', 'fonts'], function () {
 
     //监听变化
     gulp.watch([
-        'app/*.html',
-        'app/lib/**/*.*',
-        'app/styles/*.*',
-        'app/views/**/*',
-        'app/images/**/*',
-        'gom/src/**/*',
-        'gom/build/**/*',
+        'app/**/*',
+        //'app/gom/src/**/*',
+        '!app/gom/build/**/*',
+        '!app/gom/docs/**/*',
+        //'app/lib/**/*.*',
+        //'app/styles/*.*',
+        //'app/views/**/*',
+        //'app/images/**/*',
+        //'app/gom/src/**/*',
         '.tmp/fonts/**/*'
     ]).on('change', reload);
     //监听框架变化并作相应处理
-    gulp.watch('app/src/ui/ui.tmpl/*.html', ['gom']);
-    gulp.watch('gom/src/styles/*.scss', ['gom-scss']);
-    //gulp.watch('gom/src/**/*.js', ['gom']);
+    gulp.watch('app/gom/src/ui/ui.tmpl/*.html', ['gom']);
+    gulp.watch('app/gom/src/styles/*.scss', ['gom-scss']);
+    gulp.watch('app/gom/src/**/*.js', ['gom']);
     //监听APP变化并作相应处理
     gulp.watch('app/styles/*.scss', ['styles']);
-    gulp.watch('app/fonts/**/*', ['fonts']);
+    //gulp.watch('app/fonts/**/*', ['fonts']);
     //监听依赖变化并自动插入依赖
     //gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
 
-// inject bower components
-/*--------------------- DEV ----------------*/
-gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
-
-    gulp.src('app/styles/*.scss')
-        .pipe(wiredep({
-            ignorePath: /^(\.\.\/)+/
-        })).pipe(gulp.dest('app/styles'));
-
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            ignorePath: /^(\.\.\/)*\.\./
-        })).pipe(gulp.dest('app'));
-});
-
 /*--------------------- BUILD ----------------*/
 //'jshint',
-gulp.task('build', ['html', 'images', 'fonts', 'extras', 'gom'], function () {
+gulp.task('build', ['scripts', 'html', 'images', 'fonts', 'extras'], function () {
     return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
--- r-config.js
+++ r-config.js
@@ -1,11 +1,14 @@
 ({
-    appDir: 'app/scripts', // app顶级目录，非必选项。如果指定值，baseUrl则会以此为相对路径
-    baseUrl: './',       // 模块根目录。默认情况下所有模块资源都相对此目录。  若appDir值已指定，模块根目录baseUrl则相对appDir。
-    dir: '../../build',      // 指定输出目录，若值未指定，则相对 build 文件所在目录
+   //appDir: 'app/scripts', // app顶级目录，非必选项。如果指定值，baseUrl则会以此为相对路径
+    baseUrl: 'app/scripts',       // 模块根目录。默认情况下所有模块资源都相对此目录。  若appDir值已指定，模块根目录baseUrl则相对appDir。
+    dir: 'dist/scripts/',      // 指定输出目录，若值未指定，则相对 build 文件所在目录
+    paths:{
+        Gom : '../gom/build/gom'
+    },
     modules: [
         {
             name: 'app',
-            exclude: ['Gom']
+            exclude: ['Gom'],
         }
     ],
     fileExclusionRegExp: /^(r|build)\.js$/,
